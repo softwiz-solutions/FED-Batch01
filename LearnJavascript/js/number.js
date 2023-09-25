d92bc822f3c4157773aa1489006a35b658ac4dda
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-25 17:20:01 +0500

    numbers

---
+++ LearnJavascript/js/number.js
@@ -0,0 +1,22 @@
+// var score = 34.9;
+// var roundAbout = Math.round(score)
+// console.log("score=",score);
+// console.log("round", roundAbout);
+// console.log("ciel", Math.ceil(score));
+// console.log("floor", Math.floor(score));
+
+// var randomNumber = Math.random()*2;
+// console.log("randomNumber", randomNumber);
+// console.log("floor about",Math.floor(randomNumber));
+// console.log("round about",Math.round(randomNumber));
+// console.log("ceil about",Math.ceil(randomNumber));
+// console.log("random number", randomNumber));
+
+// console.log("parse int", parseInt("-9.09"));
+// console.log("parse float", parseFloat("-9.3"));
+var value = prompt("enter value");
+console.log("type", typeof (value));
+var convertedValue = Number(value)
+console.log("convertedValue", convertedValue);
+console.log("after Number()", typeof (convertedValue));
+// console.log("number");
