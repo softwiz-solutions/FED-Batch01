7b85f8f7a74f00361ba60e0e211b6c1a492451bd
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-28 14:49:08 +0500

    date and time

--- LearnJavascript/js/number.js
+++ LearnJavascript/js/number.js
@@ -44,22 +44,93 @@
 // var diff = todayYear - birthdayYear;
 // console.log("diff", diff,"today year",todayYear,"birthday year",birthdayYear);
 //******************************************** */
-
-
-
 // //  jan 1 ,1970
-var today = new Date();
+// var today = new Date();
 // var userBirthday=prompt("enter your value","June 30,2000")
-var birthday = new Date("June 30,2000");
-var todayMiliseconds = today.getTime();
-var birthdayMiliseconds = birthday.getTime();
-var diff = todayMiliseconds - birthdayMiliseconds;
-var year = diff / (1000 * 60 * 60 * 24 * 365)
-var months = diff / (1000 * 60 * 60 * 24)
-console.log("months", Math.floor(months));
+// var birthday = new Date("June 30,2000");
+// var todayMiliseconds = today.getTime();
+// var birthdayMiliseconds = birthday.getTime();
+// var diff = todayMiliseconds - birthdayMiliseconds;
+// var year = diff / (1000 * 60 * 60 * 24 * 365)
+// var months = diff / (1000 * 60 * 60 * 24)
+// console.log("months", Math.floor(months));
 // console.log("year", Math.floor(year), "mont", Math.floor(month));
-
 // console.log(Math.floor(value));
+// var currentDate = new Date();
+// console.log("current Date", currentDate.toTimeString());
+// currentDate.setFullYear(2001)
+// currentDate.setMonth(11)
+// console.log("curent data",currentDate.getFullYear());
+// setFullYear (value){
+
+// }
+// GetTime()
+// GetTime()
+// function GetTime() {
+//     var currentDate = new Date();
+//     var hour = currentDate.getHours();
+//     var minutes = currentDate.getMinutes()
+//     console.log("Hours", hour, "minutes", minutes);
+//     // var time=
+// }
+
+// SayHello()
+// const SayHello = (a,b) => {
+//     alert("hello")
+// }
+
+// GetTime();// function invoke
+// GetTime()
+// GetTime()
+
+
+
+// ************Assginment**************
+// const add = (a, b) => {
+//     result = a + b;
+//     return result
+// }
+// // if(){
+// var addition = add(num1, numb2)
+// console.log("addition", addition);
+// // }
+
+
+// *********************
+// function Sum(a, b) {// parameter
+//     var result = a + b;
+//     console.log("result", result);
+//     return result;
+// }
+
+// var addition = Sum(3, 3)// argument 4
+// if (addition > 4) {
+//     console.log("jfdkljfd");
+// }
+// console.log("addition", addition);
+// Sum(1, 4)// argument
+// Sum(9, 3)// argument
+
+
+// let sum = 3;
+// console.log("sum", sum);
+// const addition = () => {
+// sum = 2 + 3;
+// }
+// addition();
+// console.log("sum", sum);
+
+// alert("sum", sum)
+
+
+
+
+
+
+
+
+
+
 
 
 
