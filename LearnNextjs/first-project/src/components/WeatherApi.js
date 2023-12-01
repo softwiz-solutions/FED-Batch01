528f4edf13f3ce267ad085509a0ce4c9a7db9929
abdul-hanan79 <abdulhanan3279@gmail.com>
2023-12-01 15:44:22 +0500

    api

--- LearnNextjs/first-project/src/components/WeatherApi.js
+++ LearnNextjs/first-project/src/components/WeatherApi.js
@@ -4,7 +4,7 @@ import useWeatherApi from '@/customeHooks/useWeatherApi'
 import React, { useEffect } from 'react'
 
 const WeatherApi = () => {
-    const { getWeather, loader } = useWeatherApi()
+    const { getWeather, loader, city } = useWeatherApi()
     useEffect(() => {
         getWeather()
     }, [])
@@ -13,6 +13,11 @@ const WeatherApi = () => {
             <h1>
                 Weather API
             </h1>
+            {city && <div>
+                <h1>{city.name}</h1>
+                <h1>{city.main.temp}</h1>
+                <h1>{city.weather[0].main}</h1>
+            </div>}
             {loader && <h1>loading...........</h1>}
         </div>
     )
--- LearnNextjs/first-project/src/customeHooks/useWeatherApi.js
+++ LearnNextjs/first-project/src/customeHooks/useWeatherApi.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
 
 const useWeatherApi = () => {
-    console.log("process.env.API_KEY", process.env)
+    console.log("process.env.API_KEY", process.env.NEXT_PUBLIC_API_KEY)
     const [loader, setLoader] = useState(false)
+    const [city, setCity] = useState(null)
+
     // const apikey = process.env.API_KEY
-    const apiKey = "d0b5f5ca085db07c76d52c897ce8b8d0"
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY
+    const cityName = "London"
     const getWeather = async () => {
         try {
+            // console.log("process.env", process.env.NODE_ENV);
             setLoader(true)
             console.log("get weather is working");
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
             const data = await fetch(url)
-            console.log("data", data);
+            const weatherData = await data.json()
+            console.log("weather data", weatherData);
+            setCity(weatherData)
+            // console.log("data", data);
         }
         catch (error) {
             console.log("error", error.message);
@@ -23,7 +30,8 @@ const useWeatherApi = () => {
     }
     return {
         getWeather,
-        loader
+        loader,
+        city,
     }
 }
 
